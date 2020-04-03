bbb614732b1c25507ef749426eac3a70601bf9b9
Michael Tandy <mike.tandy.1@gmail.com>
2020-04-03 21:02:15 +0100

    adding error page, conditionally rendering css on results page depending on weather and time of day

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config(); // to read .env file
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
-const _ = require("lodash");
 
 /* BODY PARSER - parses our post requests so we can capture input data submitted by user */
 const bodyParser = require("body-parser");
@@ -16,7 +15,11 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
+});
+
+app.get("/error", function(req, res) {
+  res.render("error.ejs");
 });
 
 app.post("/", function(req, res) {
@@ -36,22 +39,26 @@ app.post("/", function(req, res) {
   https.get(url, function(response) {
     console.log(response.statusCode); // successful if 200
 
-    response.on("data", function(data) {
-      const weatherData = JSON.parse(data); // parses the hexadecimal data we get to JSON format
+    if (response.statusCode === 200) {
+      response.on("data", function(data) {
+        const weatherData = JSON.parse(data); // parses the hexadecimal data we get to JSON format
 
-      const temperature = weatherData.main.temp; // specify JSON path to specific data we want, store in var
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        const temperature = weatherData.main.temp; // specify JSON path to specific data we want, store in var
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      res.render("results.ejs", {
-        query: query,
-        temperature: temperature,
-        weatherDescription: weatherDescription,
-        icon: icon,
-        imageURL: imageURL
+        res.render("results.ejs", {
+          query: query,
+          temperature: temperature,
+          weatherDescription: weatherDescription,
+          icon: icon,
+          imageURL: imageURL
+        });
       });
-    });
+    } else {
+      res.render("error.ejs");
+    }
   });
 });
 
@@ -59,14 +66,4 @@ app.listen(3012, function() {
   console.log("Server is running on port 3012");
 });
 
-/* res.write(
-  "<h1>The temperature in " +
-    query +
-    " is " +
-    Math.floor(temperature) +
-    " degrees celcius.</h1>"
-);
-res.write("<h2>The forecast is " + weatherDescription + "</h2>");
-res.write("<img src='" + imageURL + "' />"); // we can use multiple res.write()s but only one res.send(), they do the same thing
-res.end(); // stops the results continously loading
-*/
+// openweather map weather conditions list: https://openweathermap.org/weather-conditions
