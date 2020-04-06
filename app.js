6fd47c9bbdbbc42a86ca5eaed0865966d5b9481a
Michael Tandy <mike.tandy.1@gmail.com>
2020-04-06 15:15:57 +0100

    sent client geolocation to server, api call using latitude and longitude working, all I need to do now is send the data back to the client to render

--- app.js
+++ app.js
@@ -11,25 +11,76 @@ const bodyParser = require("body-parser");
 const https = require("https"); // native node module that enables us to make an API call and parse the returned data.
 
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index.ejs");
 });
 
-app.get("/error", function(req, res) {
+app.get("/index2", function (req, res) {
+  res.render("index2.ejs");
+});
+
+app.get("/error", function (req, res) {
   res.render("error.ejs");
 });
 
-app.post("/", function(req, res) {
+// POST REQUEST FOR GEOLOCATION
+
+app.post("/index2", function (req, res) {
+  console.log(req.body); // I now have access to user's latitude and longitude here
+  const lat = Math.round(req.body.lat);
+  const lon = Math.round(req.body.lon);
+  console.log(lat, lon);
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    apiKey;
+
+  https.get(url, function (response) {
+    console.log(response.statusCode); // successful if 200
+
+    if (response.statusCode === 200) {
+      // the method is making a successful api call
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data); // parses the hexadecimal data we get to JSON format
+
+        const temperature = weatherData.main.temp; // specify JSON path to specific data we want, store in var
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        console.log(temperature, weatherDescription, icon, imageURL);
+
+        res.render("index2.ejs", {
+          temperature: temperature,
+          weatherDescription: weatherDescription,
+          icon: icon,
+          imageURL: imageURL,
+        });
+        console.log(icon);
+      });
+    } else {
+      res.render("index.ejs");
+    }
+  });
+});
+
+// POST REQUEST FOR FORM SUBMISSION
+
+app.post("/", function (req, res) {
   console.log(req.body.cityName); // post method captures the data inputted by user, we access that data here.
 
   const query = req.body.cityName; // user input used as query in the API call
   const apiKey = process.env.OPENWEATHER_API_KEY;
   const unit = "metric";
-  // const lat = position.coords.latitude;
-  // const lon = position.coords.longitude;
+
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     query +
@@ -38,11 +89,11 @@ app.post("/", function(req, res) {
     "&units=" +
     unit;
 
-  https.get(url, function(response) {
+  https.get(url, function (response) {
     console.log(response.statusCode); // successful if 200
 
     if (response.statusCode === 200) {
-      response.on("data", function(data) {
+      response.on("data", function (data) {
         const weatherData = JSON.parse(data); // parses the hexadecimal data we get to JSON format
 
         const temperature = weatherData.main.temp; // specify JSON path to specific data we want, store in var
@@ -55,7 +106,7 @@ app.post("/", function(req, res) {
           temperature: temperature,
           weatherDescription: weatherDescription,
           icon: icon,
-          imageURL: imageURL
+          imageURL: imageURL,
         });
         console.log(icon);
       });
@@ -65,7 +116,7 @@ app.post("/", function(req, res) {
   });
 });
 
-app.listen(3012, function() {
+app.listen(3012, function () {
   console.log("Server is running on port 3012");
 });
 
